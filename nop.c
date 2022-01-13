c29cdf93a53430ec575e7aa635df3eba3d6b0194
diegojojoayandun <3922@holbertonschool.com>
2022-01-12 21:38:19 -0500

    fix bety code style

--- nop.c
+++ nop.c
@@ -1,7 +1,12 @@
 #include "monty.h"
 
+/**
+ * nop - doesn’t do anything.
+ * @stack: struct stack_t
+ * @line_number: line number to show in error case
+*/
 void nop(**stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
