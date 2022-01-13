dbf68d7723ef7518c93a39e4a298d0a45e9b6d40
diegojojoayandun <3922@holbertonschool.com>
2022-01-13 10:39:16 -0500

    Add swap C file

--- monty.c
+++ monty.c
@@ -66,7 +66,7 @@ void match_command(char *line, char *command, stack_t **stack,
 
 	instruction_t functions[] = {
 		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
-		{"nop", nop}, {"add", add}, {NULL, NULL}
+		{"swap", swap}, {"nop", nop}, {"add", add}, {NULL, NULL}
 	};
 
 	for (i = 0; functions[i].opcode != NULL; i++)
--- monty.h
+++ monty.h
@@ -47,6 +47,7 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void match_command(char *line, char *command, stack_t **stack,
